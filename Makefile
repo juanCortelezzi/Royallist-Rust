7b0076649f5874df255781106a76894bd2fffad7
juancortelezzi <juanbicortelezzi@gmail.com>
2021-10-09 12:36:09 -0300

    added name icons

---
+++ Makefile
@@ -0,0 +1,19 @@
+build:
+	cargo build
+
+buildrel:
+	cargo build --release
+
+bench: buildrel
+	hyperfine \
+		-n rust-royal \
+		-r 800 \
+		-w 10 \
+		"./target/release/royallist ~/Documents/Web-programes/next-blog"
+
+benchcmp: buildrel
+	hyperfine \
+		-r 800 \
+		-w 10 \
+		-L TEST "./target/release/royallist,ls" \
+		"{TEST} ~/Documents/Web-programes/next-blog"
--- src/icons.rs
+++ src/icons.rs
@@ -2,51 +2,72 @@ use termcolor::Color::{self, Blue, Cyan, Green, Magenta, Red, White, Yellow};
 
 pub type Icon = (char, Color);
 
-pub fn match_ext_icon(extension: &str) -> Icon {
+pub fn match_ext_icon(extension: &str) -> Option<Icon> {
     match extension {
-        "h" => ('', Magenta),
-        "c" => ('', Magenta),
-        "jpg" => ('', Magenta),
-        "png" => ('', Magenta),
-        "vim" => ('', Green),
-        "zip" => ('', Green),
-        "scss" => ('', Red),
-        "html" => ('', Red),
-        "pdf" => ('', Red),
-        "json" => ('', Yellow),
-        "yml" => ('', Yellow),
-        "yaml" => ('', Yellow),
-        "js" => ('', Yellow),
-        "jsx" => ('', Yellow),
-        "mp4" => ('', Blue),
-        "cpp" => ('', Blue),
-        "lua" => ('', Blue),
-        "py" => ('', Blue),
-        "go" => ('ﳑ', Blue),
-        "css" => ('', Blue),
-        "ts" => ('', Blue),
-        "tsx" => ('', Blue),
-        "sh" => ('', White),
-        "txt" => ('', White),
-        "mk" => ('', White),
-        "rs" => ('', White),
-        "iso" => ('', White),
-        "md" => ('', White),
-        "mdx" => ('', White),
-        "wiki" => ('', White),
-        "mod" => ('', White),
-        "sum" => ('', White),
-        "toml" => ('', White),
-        "lock" => ('', White),
-        _ => ('', Green),
+        "h" => Some(('', Magenta)),
+        "c" => Some(('', Magenta)),
+        "jpg" => Some(('', Magenta)),
+        "png" => Some(('', Magenta)),
+        "vim" => Some(('', Green)),
+        "zip" => Some(('', Green)),
+        "scss" => Some(('', Red)),
+        "html" => Some(('', Red)),
+        "pdf" => Some(('', Red)),
+        "json" => Some(('', Yellow)),
+        "yml" => Some(('', Yellow)),
+        "yaml" => Some(('', Yellow)),
+        "js" => Some(('', Yellow)),
+        "jsx" => Some(('', Yellow)),
+        "mp4" => Some(('', Blue)),
+        "cpp" => Some(('', Blue)),
+        "lua" => Some(('', Blue)),
+        "py" => Some(('', Blue)),
+        "go" => Some(('ﳑ', Blue)),
+        "css" => Some(('', Blue)),
+        "ts" => Some(('', Blue)),
+        "tsx" => Some(('', Blue)),
+        "sh" => Some(('', White)),
+        "txt" => Some(('', White)),
+        "mk" => Some(('', White)),
+        "rs" => Some(('', White)),
+        "iso" => Some(('', White)),
+        "md" => Some(('', White)),
+        "mdx" => Some(('', White)),
+        "wiki" => Some(('', White)),
+        "mod" => Some(('', White)),
+        "sum" => Some(('', White)),
+        "toml" => Some(('', White)),
+        "lock" => Some(('', White)),
+        _ => None,
     }
 }
 
-pub fn match_dir_icon(name: &str) -> (char, Color) {
+pub fn match_name_icon(name: &str) -> Option<Icon> {
     match name {
-        ".git" => ('', Cyan),
-        "node_modules" => ('', Cyan),
-        _ => ('', Cyan),
+        "LICENSE" => Some(('ﲘ', White)),
+        "Makefile" => Some(('', White)),
+        ".gitignore" => Some(('', Magenta)),
+        _ => None,
+    }
+}
+
+pub fn match_dir_icon(name: &str) -> Option<Icon> {
+    match name {
+        ".git" => Some(('', Cyan)),
+        "node_modules" => Some(('', Cyan)),
+        _ => None,
+    }
+}
+
+pub enum PathType {
+    File,
+    Dir,
+}
+
+pub fn get_default_icon(path_type: PathType) -> Icon {
+    match path_type {
+        PathType::File => ('', Green),
+        PathType::Dir => ('', Cyan),
     }
 }
 
