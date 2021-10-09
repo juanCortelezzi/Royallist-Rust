0871ea0d318ec31b5d7e6c2b71c743f40e00ae16
juancortelezzi <juanbicortelezzi@gmail.com>
2021-10-09 00:19:26 -0300

    exctracted print logic
    improved print logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
 pub mod buffers;
 pub mod icons;
 pub mod paths;
+
+pub mod prints {
+    use crate::icons;
+    use crate::paths;
+    use std::path::Path;
+
+    pub fn common_print(path: &Path, stdout: &crate::buffers::StdoutBuffer) {
+        let mut entries = paths::read_dir(&path).expect("could not read dir");
+
+        entries.sort();
+
+        let mut filebuf = stdout.buffer();
+        let mut dirbuf = stdout.buffer();
+
+        for entry in entries {
+            let name = paths::get_filename(&entry);
+
+            if entry.as_path().is_dir() {
+                let icon = icons::match_dir_icon(&name);
+                stdout.write_entry(&mut dirbuf, icon, &name).unwrap();
+            } else {
+                let icon = icons::match_ext_icon(&paths::get_extension(&entry));
+                stdout.write_entry(&mut filebuf, icon, &name).unwrap();
+            }
+        }
+
+        stdout.flush(&dirbuf).unwrap();
+        stdout.flush(&filebuf).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use royallist::{buffers, icons, paths};
-use std::path::{Path, PathBuf};
+use royallist::{buffers, prints};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
-    #[structopt(parse(from_os_str))]
+    #[structopt(default_value = ".", parse(from_os_str))]
     files: Vec<PathBuf>,
 }
 
@@ -12,36 +12,12 @@ fn main() {
     let args = Cli::from_args();
     let stdout = buffers::StdoutBuffer::new();
     if args.files.len() == 1 {
-        common_print(&args.files[0], &stdout);
+        prints::common_print(&args.files[0], &stdout);
     } else {
         for path in args.files {
             println!("---- {} ----", path.display());
-            common_print(&path, &stdout);
+            prints::common_print(&path, &stdout);
             println!()
         }
     }
-}
-
-fn common_print(path: &Path, stdout: &buffers::StdoutBuffer) {
-    let mut entries = paths::read_dir(&path).expect("could not read dir");
-
-    entries.sort();
-
-    let mut filebuf = stdout.buffer();
-    let mut dirbuf = stdout.buffer();
-
-    for entry in entries {
-        let name = paths::get_filename(&entry);
-
-        if entry.as_path().is_dir() {
-            let icon = icons::match_dir_icon(&name);
-            stdout.write_entry(&mut dirbuf, icon, &name).unwrap();
-        } else {
-            let icon = icons::match_ext_icon(&paths::get_extension(&entry));
-            stdout.write_entry(&mut filebuf, icon, &name).unwrap();
-        }
-    }
-
-    stdout.flush(&dirbuf).unwrap();
-    stdout.flush(&filebuf).unwrap();
 }
